12f01197cff4bfef589207f72aa54c43ffb2c94d
Finn Smith <finn@timeghost.net>
2010-10-25 14:33:00 -0400

    Fix license headers.

--- plugins/reCaptcha/php/init.reCaptcha.php
+++ plugins/reCaptcha/php/init.reCaptcha.php
@@ -1,9 +1,15 @@
 <?php
-# Movable Type (r) Open Source (C) 2001-2008 Six Apart, Ltd.
-# This program is distributed under the terms of the
-# GNU General Public License, version 2.
-#
-# $Id: init.reCaptcha.php 1174 2008-01-08 21:02:50Z bchoate $
+##############################################################################
+# Copyright © 2010 Six Apart Ltd.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details.  You should have received a copy of the GNU
+# General Public License version 2 along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
 
 require_once("captcha_lib.php");
 
--- plugins/reCaptcha/reCaptcha.pl
+++ plugins/reCaptcha/reCaptcha.pl
@@ -1,12 +1,14 @@
-# Movable Type (r) Open Source (C) 2005-2008 Six Apart, Ltd.
-# This program is distributed under the terms of the
-# GNU General Public License, version 2.
-#
-# $Id: reCaptcha.pl 1174 2008-01-08 21:02:50Z bchoate $
-
-# reCaptcha plugin for Movable Type
-# Author: Six Apart (http://www.sixapart.com)
-# Released under the Artistic and GPLv2 License
+##############################################################################
+# Copyright © 2010 Six Apart Ltd.
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details.  You should have received a copy of the GNU
+# General Public License version 2 along with this program. If not, see
+# <http://www.gnu.org/licenses/>.
 
 package MT::Plugin::reCaptcha;
 
